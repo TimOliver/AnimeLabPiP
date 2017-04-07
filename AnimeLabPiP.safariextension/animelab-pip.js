7cb65c997e5290ba3fe2a4e6bb4a4b22a253c759
Tim Oliver <me@timoliver.com.au>
2017-04-07 01:31:42 -0700

    Cleaned up code structure

--- AnimeLabPiP.safariextension/animelab-pip.js
+++ AnimeLabPiP.safariextension/animelab-pip.js
@@ -1,5 +1,32 @@
 
+//
+//  animelab-pip.js
+//
+//  Copyright 2017 Timothy Oliver. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 (function () {
+
+	var previousVideoThumbnailURL = "";
+	var currentVideoThumbnailURL = "";
+	var nextVideoThumbnailURL = "";
+
 	var setUpBackgroundOverlay = function() {
 		var backgroundContainer = document.querySelector("#pip-background-overlay-container");
 		if (backgroundContainer != null) { return; }
@@ -18,22 +45,15 @@
 		
 		// Grab the metadata image for this episode and attach it to the overlay
 		// (This should preload the image data so it'll be ready by the time we need it)
-		var thumbnailElement = document.querySelector('[property="og:image"]');
-		if (thumbnailElement == null) { return; }
-		var thumbnailURL = 'http:' + thumbnailElement.content;
+		// var thumbnailElement = document.querySelector('[property="og:image"]');
+		// if (thumbnailElement == null) { return; }
+		// var thumbnailURL = 'http:' + thumbnailElement.content;
 		
-		// Apply the thumbnail image to our image element
-		imageElement.style.backgroundImage = "url('" + thumbnailURL + "')";
+		// // Apply the thumbnail image to our image element
+		// imageElement.style.backgroundImage = "url('" + thumbnailURL + "')";
 	};
 
-	var contentLoadedCallback = function(event) {
-		// Confirm we're on the player page before proceeding
-		if (!/animelab\.com\/player\//i.test(window.location.href)) { return; }
-
-		// Create a dummy video element to test if we can indeed do PiP mode
-		var video = document.createElement("video");
-		if (!video.webkitSupportsPresentationMode) { return; }
-
+	var setUpPiPButton = function() {
 		// Find the player container
 		var container = document.querySelector('.video-controls');
 		if (!container) { return; }
@@ -45,18 +65,53 @@
 		pipButton.setAttribute("data-title", "Toggle PiP");
 		pipButton.setAttribute("data-placement", "top");
 		pipButton.addEventListener("click", function(event) {
-			var video = document.querySelector(".jw-video");
-			var inPiP = (video.webkitPresentationMode === "picture-in-picture");
-        	video.webkitSetPresentationMode(inPiP ? "inline" : "picture-in-picture");
+			var video = document.querySelector("video.jw-video");
+			video.webkitSetPresentationMode(video.webkitPresentationMode == "inline" ? "picture-in-picture" : "inline");
    		});
 
 		// Append to the container controller
 		container.appendChild(pipButton);
+	};
+
+	var videoContainerDidMutate = function(mutations) {
+		mutations.forEach(function(mutation) {
+    		var node = mutation.addedNodes.item(0);
+    		if (node == null) { return; }
+    		if (node.className != '') {
+    			// Update wallpaper
+    		}
+  		});
+	};
+
+	var setUpVideoContainerObserver = function() {
+		// The video wrapper div is present in the initial DOM, but is initially empty.
+		// Register an observer to detect when the video component is set up.
+		var target = document.querySelector('#video-component-wrapper');
+		
+		// Create the observer and attach our callback function
+		var observer = new MutationObserver(videoContainerDidMutate);
+		var config = { childList: true };
+		observer.observe(target, config);
+	};
+
+	var didLoadDomContent = function(event) {
+		// Confirm we're on the player page before proceeding
+		if (!/animelab\.com\/player\//i.test(window.location.href)) { return; }
+
+		// Test to see if video tags in this version of Safari support PiP
+		var video = document.createElement("video");
+		if (!video.webkitSupportsPresentationMode) { return; }
+
+		// Create a PiP button and attach it to the player controls
+		setUpPiPButton();
 
 		// Set up the background view visible while PiP is active
 		setUpBackgroundOverlay();
+
+		// Set up the video container observer
+		setUpVideoContainerObserver();
 	};
 
-	// Detect when the HTML content has completed loading (but don't wait for images/media etc)
-	document.addEventListener("DOMContentLoaded", contentLoadedCallback);
+	// Detect when the HTML content has completed loading
+	document.addEventListener("DOMContentLoaded", didLoadDomContent);
 }());
